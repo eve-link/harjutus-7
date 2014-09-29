36f699cf84975006993af3214822d0a847c0cf2b
eve-link <eve.link.1992@gmail.com>
2014-09-29 22:23:39 +0300

    for loop

--- harjutus-7.php
+++ harjutus-7.php
@@ -70,23 +70,75 @@
 
       <h3>Conditional statements in while loop</h3>
 
-  <?php
-    $count = 1;
+      <?php
+      $count = 1;
 
-    while ($count <=10) {
-        if ($count == 3) {
-            echo "kolm";
-        } elseif($count == 7) {
-            echo "seitse";
-            
-        } else {
-        	echo $count;
-        }
+      while ($count <=10) {
+      	if ($count == 3) {
+      		echo "kolm";
+      	} elseif($count == 7) {
+      		echo "seitse";
 
-        $count += 1;
+      	} else {
+      		echo $count;
+      	}
+
+      	$count += 1;
+      }
+      ?>
+
+      // While tsükkel
+      <?php
+      $count = 1;
+
+      while ($count <= 10) {
+      	echo $count;
+      	$count += 1;
+      }
+      ?>
+
+      // For tsükkel
+      <?php
+      for ($count = 1; $count <= 10; $count += 1) {
+      	echo $count;
+      }
+      ?> 
+
+      <h2>For loop</h2>
+
+      <?php
+      for ($count = 1; $count <= 20; $count += 1) {
+      	if ($count % 2 == 0) {
+      		echo $count . ' on paarisarv.<br>';}
+      		else {echo $count . ' on paaritu arv.<br>';}
+      	}
+      	?> 
+      	<br>
+      	// While loop
+      	<?php
+      	$students = 1;
+
+      	while ($students <= 20) {
+        // Ulata õpilasele paberileht.
+
+      		$students++;
+      	}
+      	?>
+
+      	<?php
+      	for ($students = 1; $students <= 20; $students++) {
+        // Ulata õpilasele paberileht.
+      	}
+      	?>
+
+<ul><?php
+
+    $drinks = array("tea", "coffee", "water", "sprite");
+    foreach ($drinks as $drink) {
+       echo "<li>" . $drink . "</li>";
     }
 ?>
+<ul>
 
-
-    </body>
-    </html> 
+      </body>
+      </html> 
