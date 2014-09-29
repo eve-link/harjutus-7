4d91227d9efc2291a9a609d03d9bb6fabf329285
eve-link <eve.link.1992@gmail.com>
2014-09-29 22:59:55 +0300

    foreach loop table

--- harjutus-7.php
+++ harjutus-7.php
@@ -43,12 +43,12 @@
   $number = 10;
 
   while ($count < 10) {
-     while ($number < 0) {
-          echo $number;
-          $number -= 1;
-    }
+       while ($number < 0) {
+        echo $number;
+        $number -= 1;
+  }
 
-    $count += 1;
+  $count += 1;
 }
 ?>
 <br><br>
@@ -57,13 +57,13 @@
 $count = 0;
 
 while ($count < 15) {
-     if ($count == 5) {
-          echo "<script>alert('Number viie kord');</script>";
-    } else {
-          echo $count;
-    }
+ if ($count == 5) {
+  echo "<script>alert('Number viie kord');</script>";
+} else {
+  echo $count;
+}
 
-    $count += 1;
+$count += 1;
 }
 ?>
 <br><br>
@@ -74,16 +74,16 @@ while ($count < 15) {
 $count = 1;
 
 while ($count <=10) {
-     if ($count == 3) {
-          echo "kolm";
-    } elseif($count == 7) {
-          echo "seitse";
+ if ($count == 3) {
+  echo "kolm";
+} elseif($count == 7) {
+  echo "seitse";
 
-    } else {
-          echo $count;
-    }
+} else {
+  echo $count;
+}
 
-    $count += 1;
+$count += 1;
 }
 ?>
 
@@ -92,15 +92,15 @@ while ($count <=10) {
 $count = 1;
 
 while ($count <= 10) {
-     echo $count;
-     $count += 1;
+ echo $count;
+ $count += 1;
 }
 ?>
 
 // For tsükkel
 <?php
 for ($count = 1; $count <= 10; $count += 1) {
-     echo $count;
+ echo $count;
 }
 ?> 
 
@@ -108,34 +108,34 @@ for ($count = 1; $count <= 10; $count += 1) {
 
 <?php
 for ($count = 1; $count <= 20; $count += 1) {
-     if ($count % 2 == 0) {
-          echo $count . ' on paarisarv.<br>';}
-          else {echo $count . ' on paaritu arv.<br>';}
-    }
-    ?> 
-    <br>
-    // While loop
-    <?php
-    $students = 1;
+ if ($count % 2 == 0) {
+  echo $count . ' on paarisarv.<br>';}
+  else {echo $count . ' on paaritu arv.<br>';}
+}
+?> 
+<br>
+// While loop
+<?php
+$students = 1;
 
-    while ($students <= 20) {
+while ($students <= 20) {
         // Ulata õpilasele paberileht.
 
-          $students++;
-    }
-    ?>
+  $students++;
+}
+?>
 
-    <?php
-    for ($students = 1; $students <= 20; $students++) {
+<?php
+for ($students = 1; $students <= 20; $students++) {
         // Ulata õpilasele paberileht.
-    }
-    ?>
+}
+?>
 
-    <ul><?php
+<ul><?php
 
-    $drinks = array("tea", "coffee", "water", "sprite");
-    foreach ($drinks as $drink) {
-     echo "<li>" . $drink . "</li>";
+$drinks = array("tea", "coffee", "water", "sprite");
+foreach ($drinks as $drink) {
+ echo "<li>" . $drink . "</li>";
 }
 ?>
 </ul>
@@ -143,32 +143,69 @@ for ($count = 1; $count <= 20; $count += 1) {
 <h2>Foreach loop</h2>
 
 <ul>
-<?php 
- $businesses = array("Nike", "Adidas", "Puma", "Reebok", "Oneill");
-    foreach ($businesses as $business) {
-     echo "<li>" . $business . "</li>";
-}
-?>
+      <?php 
+      $businesses = array("Nike", "Adidas", "Puma", "Reebok", "Oneill");
+      foreach ($businesses as $business) {
+       echo "<li>" . $business . "</li>";
+ }
+ ?>
 </ul>
 
 <ol>
-<?php 
- $classes = array("3D graafika", "Sisuhaldussüsteemid", "Veebikujundus", "Veebitehnoloogiad", "Serveripoolsete rakenduste programmeerimine");
-    foreach ($classes as $classes) {
-     echo "<li>" . $classes . "</li>";
-}
-?>
+      <?php 
+      $classes = array("3D graafika", "Sisuhaldussüsteemid", "Veebikujundus", "Veebitehnoloogiad", "Serveripoolsete rakenduste programmeerimine");
+      foreach ($classes as $classes) {
+       echo "<li>" . $classes . "</li>";
+ }
+ ?>
 </ol>
 
 
-<ul>
-<?php 
- $info = array("Nike", "Adidas", "Puma", "Reebok", "Oneill");
-    foreach ($businesses as $business) {
-     echo "<li>" . $business . "</li>";
+<?php
+$contact = array(
+    "first_name" => "Eve",
+    "phone_number" => 53334325,
+    "location" => "Tartu"
+    );
+
+foreach ($contact as $attribute => $value ) {
+    echo "{$attribute}: {$value}<br>";
 }
 ?>
-</ul>
+<br>
 
+<DOCTYPE html>
+      <html>
+      <head>
+      </head>
+      <body cz-shortcut-listen="true">
+            <table>
+                  <tbody>
+                        <contact-table>
+                        <tr>
+                              <td>first_name</td>
+                              <td>Eve</td>
+                        </tr>
+                        <tr>
+                              <td>last_name</td>
+                              <td>Link</td>
+                        </tr>
+                        <tr>
+                              <td>e-mail</td>
+                              <td>eve.link@khk.ee</td>
+                        </tr>
+                        <tr>
+                              <td>age</td>
+                              <td>22</td>
+                        </tr>
+                  </contact-table>
+            </tbody>
+      </table>
 </body>
-</html> 
+</html>
+
+<style>
+table { border: 1px solid red; }
+tr { background-color: gray; }
+tbody { padding: 5px; }
+</style>
