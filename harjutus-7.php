e54855342cf220d54f785726170ad62a548b3a02
eve-link <eve.link.1992@gmail.com>
2014-09-29 22:35:10 +0300

    foreach loop

--- harjutus-7.php
+++ harjutus-7.php
@@ -35,110 +35,140 @@
     while ($count <= 10) {
         echo $count;
         $count = 1;
-      }*/
-      ?> 
+  }*/
+  ?> 
 
-      <?php
-      $count = 1;
-      $number = 10;
+  <?php
+  $count = 1;
+  $number = 10;
 
-      while ($count < 10) {
-      	while ($number < 0) {
-      		echo $number;
-      		$number -= 1;
-      	}
+  while ($count < 10) {
+     while ($number < 0) {
+          echo $number;
+          $number -= 1;
+    }
 
-      	$count += 1;
-      }
-      ?>
-      <br><br>
+    $count += 1;
+}
+?>
+<br><br>
 
-      <?php
-      $count = 0;
+<?php
+$count = 0;
 
-      while ($count < 15) {
-      	if ($count == 5) {
-      		echo "<script>alert('Number viie kord');</script>";
-      	} else {
-      		echo $count;
-      	}
+while ($count < 15) {
+     if ($count == 5) {
+          echo "<script>alert('Number viie kord');</script>";
+    } else {
+          echo $count;
+    }
 
-      	$count += 1;
-      }
-      ?>
-      <br><br>
+    $count += 1;
+}
+?>
+<br><br>
 
-      <h3>Conditional statements in while loop</h3>
+<h3>Conditional statements in while loop</h3>
 
-      <?php
-      $count = 1;
+<?php
+$count = 1;
 
-      while ($count <=10) {
-      	if ($count == 3) {
-      		echo "kolm";
-      	} elseif($count == 7) {
-      		echo "seitse";
+while ($count <=10) {
+     if ($count == 3) {
+          echo "kolm";
+    } elseif($count == 7) {
+          echo "seitse";
 
-      	} else {
-      		echo $count;
-      	}
+    } else {
+          echo $count;
+    }
 
-      	$count += 1;
-      }
-      ?>
+    $count += 1;
+}
+?>
 
-      // While tsükkel
-      <?php
-      $count = 1;
+// While tsükkel
+<?php
+$count = 1;
 
-      while ($count <= 10) {
-      	echo $count;
-      	$count += 1;
-      }
-      ?>
+while ($count <= 10) {
+     echo $count;
+     $count += 1;
+}
+?>
 
-      // For tsükkel
-      <?php
-      for ($count = 1; $count <= 10; $count += 1) {
-      	echo $count;
-      }
-      ?> 
+// For tsükkel
+<?php
+for ($count = 1; $count <= 10; $count += 1) {
+     echo $count;
+}
+?> 
 
-      <h2>For loop</h2>
+<h2>For loop</h2>
 
-      <?php
-      for ($count = 1; $count <= 20; $count += 1) {
-      	if ($count % 2 == 0) {
-      		echo $count . ' on paarisarv.<br>';}
-      		else {echo $count . ' on paaritu arv.<br>';}
-      	}
-      	?> 
-      	<br>
-      	// While loop
-      	<?php
-      	$students = 1;
+<?php
+for ($count = 1; $count <= 20; $count += 1) {
+     if ($count % 2 == 0) {
+          echo $count . ' on paarisarv.<br>';}
+          else {echo $count . ' on paaritu arv.<br>';}
+    }
+    ?> 
+    <br>
+    // While loop
+    <?php
+    $students = 1;
 
-      	while ($students <= 20) {
+    while ($students <= 20) {
         // Ulata õpilasele paberileht.
 
-      		$students++;
-      	}
-      	?>
+          $students++;
+    }
+    ?>
 
-      	<?php
-      	for ($students = 1; $students <= 20; $students++) {
+    <?php
+    for ($students = 1; $students <= 20; $students++) {
         // Ulata õpilasele paberileht.
-      	}
-      	?>
+    }
+    ?>
 
-<ul><?php
+    <ul><?php
 
     $drinks = array("tea", "coffee", "water", "sprite");
     foreach ($drinks as $drink) {
-       echo "<li>" . $drink . "</li>";
-    }
+     echo "<li>" . $drink . "</li>";
+}
 ?>
-<ul>
+</ul>
 
-      </body>
-      </html> 
+<h2>Foreach loop</h2>
+
+<ul>
+<?php 
+ $businesses = array("Nike", "Adidas", "Puma", "Reebok", "Oneill");
+    foreach ($businesses as $business) {
+     echo "<li>" . $business . "</li>";
+}
+?>
+</ul>
+
+<ol>
+<?php 
+ $classes = array("3D graafika", "Sisuhaldussüsteemid", "Veebikujundus", "Veebitehnoloogiad", "Serveripoolsete rakenduste programmeerimine");
+    foreach ($classes as $classes) {
+     echo "<li>" . $classes . "</li>";
+}
+?>
+</ol>
+
+
+<ul>
+<?php 
+ $info = array("Nike", "Adidas", "Puma", "Reebok", "Oneill");
+    foreach ($businesses as $business) {
+     echo "<li>" . $business . "</li>";
+}
+?>
+</ul>
+
+</body>
+</html> 
